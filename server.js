a218f240c9c70973fa30fc6e40dbc009f4241971
JustKoru <koru@justkoru.com>
2023-05-21 19:01:34 +0200

    Ngrok compatibility

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const Discord = require('discord.js');
 const webhookClient = new Discord.WebhookClient({ id: process.env.DISCORD_WEBHOOK_ID, token: process.env.DISCORD_WEBHOOK_TOKEN });
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const ngrokenabled = true;
 const dcclient = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -27,7 +28,6 @@ dcclient.once(Events.ClientReady, c => {
         status: 'dnd'
       });
 });
-
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -458,5 +458,13 @@ dcclient.on('messageCreate', message => {
 http.listen(2345, () => {
     console.log('listening on *:2345');
 });
+
+if (ngrokenabled) {
+const ngrok = require('ngrok');
+(async function() {
+    const url = await ngrok.connect(2345);
+    console.log(url);
+})();
+}
 
 dcclient.login(token);
